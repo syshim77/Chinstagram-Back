54a6998be048b3ae13197b8172136b6920499690
sooyeon Shim <syshim77@sooyeonui-MacBook-Pro.local>
2020-07-22 22:03:31 +0900

    edit image model

--- Chinstagram/src/main/java/com/landvibe/chinstagram/models/Image.java
+++ Chinstagram/src/main/java/com/landvibe/chinstagram/models/Image.java
@@ -1,14 +1,14 @@
 package com.landvibe.chinstagram.models;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
